3e8930c2906d011d1f5e9a31f0091e6951342f50
Spandan Barve <spandan567@gmail.com>
2024-10-24 17:53:38 +0530

    run deno fmt

--- packages/contracts/src/Lock.sol.test.ts
+++ packages/contracts/src/Lock.sol.test.ts
@@ -4,37 +4,37 @@ import runtime from "../runtime.local.ts"
 import { expect } from "@std/expect"
 
 async function deployOneYearLockFixture() {
-	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
 
-	const lockedAmount = parseGwei("1")
-	const unlockTime = (await runtime.block.time.latest()) +
-		BigInt(ONE_YEAR_IN_SECS)
+  const lockedAmount = parseGwei("1")
+  const unlockTime = (await runtime.block.time.latest()) +
+    BigInt(ONE_YEAR_IN_SECS)
 
-	const [owner, otherAccount] = runtime.clients
+  const [owner, otherAccount] = runtime.clients
 
-	const lock = await runtime.deployContract({
-		abi: Lock.abi,
-		bytecode: Lock.bytecode as Address,
-		value: lockedAmount,
-		args: [unlockTime],
-	})
+  const lock = await runtime.deployContract({
+    abi: Lock.abi,
+    bytecode: Lock.bytecode as Address,
+    value: lockedAmount,
+    args: [unlockTime],
+  })
 
-	const publicClient = runtime.publicClient
+  const publicClient = runtime.publicClient
 
-	return {
-		lock,
-		unlockTime,
-		lockedAmount,
-		owner,
-		otherAccount,
-		publicClient,
-	}
+  return {
+    lock,
+    unlockTime,
+    lockedAmount,
+    owner,
+    otherAccount,
+    publicClient,
+  }
 }
 
 Deno.test("Should set the right unlockTime", async () => {
-	const { lock, unlockTime } = await runtime.loadFixture(
-		deployOneYearLockFixture,
-	)
+  const { lock, unlockTime } = await runtime.loadFixture(
+    deployOneYearLockFixture,
+  )
 
-	expect(await lock.read.unlockTime([])).toEqual(unlockTime)
+  expect(await lock.read.unlockTime([])).toEqual(unlockTime)
 })
