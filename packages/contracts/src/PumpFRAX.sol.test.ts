ab0e57a0f6c6e24b529f26d55776f20693042234
Spandan Barve <spandan567@gmail.com>
2024-10-27 02:20:50 +0530

    Add metaTxReq utility function

--- packages/contracts/src/PumpFRAX.sol.test.ts
+++ packages/contracts/src/PumpFRAX.sol.test.ts
@@ -5,94 +5,94 @@ import * as viem from "viem";
 import { metaTxRequest, parseFrax } from "../utils.ts";
 
 async function deployFixture() {
-    const [owner, forwarder, acc2] = runtime.clients;
+  const [owner, forwarder, acc2] = runtime.clients;
 
-    const registry = await runtime.deployContract("ForwarderRegistry", []);
-    const pFrax = await runtime.deployContract("PumpFRAX", [registry.address]);
-    await registry.write.addTrustedExecutor([pFrax.address]);
-    await registry.write.registerForwarder([forwarder.account.address]);
+  const registry = await runtime.deployContract("ForwarderRegistry", []);
+  const pFrax = await runtime.deployContract("PumpFRAX", [registry.address]);
+  await registry.write.addTrustedExecutor([pFrax.address]);
+  await registry.write.registerForwarder([forwarder.account.address]);
 
-    const publicClient = runtime.publicClient;
+  const publicClient = runtime.publicClient;
 
-    async function mint(client: viem.WalletClient, amount: number) {
-        if (!client.account) {
-            throw new Error("Invalid client account, unable to mint");
-        }
-        await pFrax.write.mint([client.account.address, parseFrax(amount)]);
+  async function mint(client: viem.WalletClient, amount: number) {
+    if (!client.account) {
+      throw new Error("Invalid client account, unable to mint");
     }
+    await pFrax.write.mint([client.account.address, parseFrax(amount)]);
+  }
 
-    return {
-        owner,
-        forwarder,
-        acc2,
-        registry,
-        pFrax,
-        publicClient,
-        mint,
-    };
+  return {
+    owner,
+    forwarder,
+    acc2,
+    registry,
+    pFrax,
+    publicClient,
+    mint,
+  };
 }
 
 Deno.test("Should have initial supply of zero", async () => {
-    const { pFrax } = await runtime.loadFixture(deployFixture);
+  const { pFrax } = await runtime.loadFixture(deployFixture);
 
-    expect(await pFrax.read.totalSupply()).toBe(0n);
+  expect(await pFrax.read.totalSupply()).toBe(0n);
 });
 
 Deno.test("metaTx: transfer", async () => {
-    const { pFrax, owner, registry, forwarder, acc2: holder, mint } =
-        await runtime
-            .loadFixture(deployFixture);
+  const { pFrax, owner, registry, forwarder, acc2: holder, mint } =
+    await runtime
+      .loadFixture(deployFixture);
 
-    await mint(holder, 100);
+  await mint(holder, 100);
 
-    //   const msg = viem.keccak256(
-    //     viem.encodePacked(["address", "uint"], [
-    //       owner.account.address,
-    //       parseFrax(50),
-    //     ]),
-    //   );
+  //   const msg = viem.keccak256(
+  //     viem.encodePacked(["address", "uint"], [
+  //       owner.account.address,
+  //       parseFrax(50),
+  //     ]),
+  //   );
 
-    //   const digest = viem.keccak256(
-    //     viem.encodePacked(["address", "address", "string", "bytes32", "uint"], [
-    //       holder.account.address,
-    //       pFrax.address,
-    //       "transfer",
-    //       msg,
-    //       0n,
-    //     ]),
-    //   );
+  //   const digest = viem.keccak256(
+  //     viem.encodePacked(["address", "address", "string", "bytes32", "uint"], [
+  //       holder.account.address,
+  //       pFrax.address,
+  //       "transfer",
+  //       msg,
+  //       0n,
+  //     ]),
+  //   );
 
-    //   const sig = await holder.signMessage({
-    //     message: { raw: viem.hexToBytes(digest) },
-    //   });
+  //   const sig = await holder.signMessage({
+  //     message: { raw: viem.hexToBytes(digest) },
+  //   });
 
-    const req = await metaTxRequest({
-        contract: pFrax.address,
-        signer: holder,
-        functionName: "transfer",
-        args: [
-            ["address", "uint"],
-            [owner.account.address, parseFrax(50)] as const,
-        ],
-    });
+  const req = await metaTxRequest({
+    contract: pFrax.address,
+    signer: holder,
+    functionName: "transfer",
+    args: [
+      ["address", "uint"],
+      [owner.account.address, parseFrax(50)] as const,
+    ],
+  });
 
-    const nonceBefore = await registry.read.nonceOf([holder.account.address]);
+  const nonceBefore = await registry.read.nonceOf([holder.account.address]);
 
-    await pFrax.write.metaTransfer(req, {
-        account: forwarder.account,
-    });
+  await pFrax.write.metaTransfer(req, {
+    account: forwarder.account,
+  });
 
-    await runtime.expectContractFunctionExecutionError(
-        pFrax.write.metaTransfer(req, {
-            account: forwarder.account,
-        }),
-        "Invalid Signature or Invalid Execution Request",
-    );
+  await runtime.expectContractFunctionExecutionError(
+    pFrax.write.metaTransfer(req, {
+      account: forwarder.account,
+    }),
+    "Invalid Signature or Invalid Execution Request",
+  );
 
-    expect(await registry.read.nonceOf([holder.account.address])).toBe(
-        nonceBefore + 1n,
-    );
-    expect(await pFrax.read.balanceOf([owner.account.address])).toBe(
-        parseFrax(50),
-    );
+  expect(await registry.read.nonceOf([holder.account.address])).toBe(
+    nonceBefore + 1n,
+  );
+  expect(await pFrax.read.balanceOf([owner.account.address])).toBe(
+    parseFrax(50),
+  );
 });
--- packages/contracts/utils.ts
+++ packages/contracts/utils.ts
@@ -1,38 +1,38 @@
 import * as viem from "viem";
 
 export function parseFrax(value: number | bigint) {
-    return BigInt(Math.pow(Number(value), 18));
+  return BigInt(Math.pow(Number(value), 18));
 }
 
 export async function metaTxRequest<
-    A extends Readonly<Parameters<typeof viem.encodePacked>>,
+  A extends Readonly<Parameters<typeof viem.encodePacked>>,
 >(
-    parameters: {
-        signer: viem.WalletClient<viem.Transport, viem.Chain, viem.Account>;
-        contract: viem.Address;
-        functionName: string;
-        args: A;
-    },
+  parameters: {
+    signer: viem.WalletClient<viem.Transport, viem.Chain, viem.Account>;
+    contract: viem.Address;
+    functionName: string;
+    args: A;
+  },
 ) {
-    const { signer, args, contract, functionName } = parameters;
+  const { signer, args, contract, functionName } = parameters;
 
-    const msg = viem.keccak256(
-        viem.encodePacked(args[0], args[1]),
-    );
+  const msg = viem.keccak256(
+    viem.encodePacked(args[0], args[1]),
+  );
 
-    const digest = viem.keccak256(
-        viem.encodePacked(["address", "address", "string", "bytes32", "uint"], [
-            signer.account.address,
-            contract,
-            functionName.toString(),
-            msg,
-            0n,
-        ]),
-    );
+  const digest = viem.keccak256(
+    viem.encodePacked(["address", "address", "string", "bytes32", "uint"], [
+      signer.account.address,
+      contract,
+      functionName.toString(),
+      msg,
+      0n,
+    ]),
+  );
 
-    const signature = await signer.signMessage({
-        message: { raw: viem.hexToBytes(digest) },
-    });
+  const signature = await signer.signMessage({
+    message: { raw: viem.hexToBytes(digest) },
+  });
 
-    return [signer.account.address, ...(args[1] as A[1]), signature] as const;
+  return [signer.account.address, ...(args[1] as A[1]), signature] as const;
 }
