87c20d19339796da4c64de8357d6c68cc171f849
Spandan Barve <spandan567@gmail.com>
2024-10-30 15:49:36 +0530

    optimized compiler enable

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,7 +1,10 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: { optimizer: { enabled: true, runs: 200 } },
+  },
   paths: {
     sources: "./src",
   },
