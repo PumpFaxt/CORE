815920efddf9a9e74122341a5261364f213b64d1
Spandan Barve <spandan567@gmail.com>
2024-11-02 21:11:00 +0530

    deno fmt run

--- packages/contracts/src/PumpfaxtToken.sol.test.ts
+++ packages/contracts/src/PumpfaxtToken.sol.test.ts
@@ -10,70 +10,70 @@ const TOKEN_URI = "ipfs://test";
 const ONE_TOKEN = 1_000_000_000_000_000_000n;
 
 async function deployFixture() {
-    const { owner, master, acc1, acc2, pFrax } = await runtime
-        .loadFixture(
-            setupFixture,
-        );
-
-    await master.write.launchToken(
-        [TOKEN_NAME, TOKEN_SYMBOL, TOKEN_URI],
+  const { owner, master, acc1, acc2, pFrax } = await runtime
+    .loadFixture(
+      setupFixture,
     );
 
-    const logs = await runtime.readContractEvents(master, "Launch");
-    const tokenAddress = logs[0].args.token;
+  await master.write.launchToken(
+    [TOKEN_NAME, TOKEN_SYMBOL, TOKEN_URI],
+  );
 
-    if (tokenAddress === undefined || !viem.isAddress(tokenAddress)) {
-        throw new Error("Failed to deploy token");
-    }
+  const logs = await runtime.readContractEvents(master, "Launch");
+  const tokenAddress = logs[0].args.token;
 
-    const token = runtime.getContract("PumpfaxtToken", tokenAddress);
+  if (tokenAddress === undefined || !viem.isAddress(tokenAddress)) {
+    throw new Error("Failed to deploy token");
+  }
 
-    // Give everyone pFrax and approve the master
-    await pFrax.write.buy([owner.account.address, parseFrax(100_000)]);
-    await pFrax.write.buy([acc1.account.address, parseFrax(100_000)]);
-    await pFrax.write.buy([acc2.account.address, parseFrax(100_000)]);
+  const token = runtime.getContract("PumpfaxtToken", tokenAddress);
 
-    await pFrax.write.approve([master.address, parseFrax(100_000)], {
-        account: owner.account,
-    });
-    await pFrax.write.approve([master.address, parseFrax(100_000)], {
-        account: acc1.account,
-    });
-    await pFrax.write.approve([master.address, parseFrax(100_000)], {
-        account: acc2.account,
-    });
+  // Give everyone pFrax and approve the master
+  await pFrax.write.buy([owner.account.address, parseFrax(100_000)]);
+  await pFrax.write.buy([acc1.account.address, parseFrax(100_000)]);
+  await pFrax.write.buy([acc2.account.address, parseFrax(100_000)]);
 
-    return {
-        owner,
-        acc1,
-        acc2,
-        token,
-        master,
-    };
+  await pFrax.write.approve([master.address, parseFrax(100_000)], {
+    account: owner.account,
+  });
+  await pFrax.write.approve([master.address, parseFrax(100_000)], {
+    account: acc1.account,
+  });
+  await pFrax.write.approve([master.address, parseFrax(100_000)], {
+    account: acc2.account,
+  });
+
+  return {
+    owner,
+    acc1,
+    acc2,
+    token,
+    master,
+  };
 }
 
 Deno.test("Should deploy with correct parameters", async () => {
-    const { token } = await runtime.loadFixture(deployFixture);
+  const { token } = await runtime.loadFixture(deployFixture);
 
-    expect(await token.read.name()).toBe(TOKEN_NAME);
-    expect(await token.read.symbol()).toBe(TOKEN_SYMBOL);
-    expect(await token.read.uri()).toBe(TOKEN_URI);
+  expect(await token.read.name()).toBe(TOKEN_NAME);
+  expect(await token.read.symbol()).toBe(TOKEN_SYMBOL);
+  expect(await token.read.uri()).toBe(TOKEN_URI);
 });
 
 Deno.test("Should have correct initial supply (PumpfaxtMaster.newTokenStartingSupply) and mint all to itself", async () => {
-    const { token, master } = await runtime.loadFixture(deployFixture);
+  const { token, master } = await runtime.loadFixture(deployFixture);
 
-    const supply = await token.read.totalSupply();
+  const supply = await token.read.totalSupply();
 
-    expect(supply / ONE_TOKEN).toBe(
-        await master.read.newTokenStartingSupply(),
-    );
+  expect(supply / ONE_TOKEN).toBe(
+    await master.read.newTokenStartingSupply(),
+  );
 
-    expect(await token.read.balanceOf([token.address])).toBe(supply);
+  expect(await token.read.balanceOf([token.address])).toBe(supply);
 });
 
 Deno.test("Should start trading with a non zero price", async () => {
-    const { token } = await runtime.loadFixture(deployFixture);
+  const { token } = await runtime.loadFixture(deployFixture);
 
-    expect(await token.read.tokenPrice()).toBeGreaterThan(0);
+  expect(await token.read.tokenPrice()).toBeGreaterThan(0);
 });
