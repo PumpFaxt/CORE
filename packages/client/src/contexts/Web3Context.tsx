471e5397f831f8f45040887a07087ba5a536aa39
Spandan Barve <spandan567@gmail.com>
2024-11-04 02:47:58 +0530

    Add tailwind and privy to client

--- packages/client/src/contexts/Web3Context.tsx
+++ packages/client/src/contexts/Web3Context.tsx
@@ -2,35 +2,35 @@ import { PrivyProvider } from "@privy-io/react-auth";
 import { fraxtalTestnet } from "viem/chains";
 
 export default function Web3Provider(
-    { children }: { children: preact.VNode },
+  { children }: { children: preact.VNode },
 ) {
-    return (
-        <PrivyProvider
-            appId="cm31r4s7n04ed120svzg37zc3"
-            config={{
-                appearance: {
-                    theme: "dark",
-                    accentColor: "#676FFF",
-                    logo: "https://your-logo-url",
-                },
-                supportedChains: [fraxtalTestnet],
-                intl: { defaultCountry: "IN" },
-                loginMethods: [
-                    "email",
-                    "wallet",
-                    "google",
-                    "apple",
-                    // "telegram",
-                    "discord",
-                    "twitter",
-                    "farcaster",
-                ],
-                embeddedWallets: {
-                    createOnLogin: "users-without-wallets",
-                },
-            }}
-        >
-            {children}
-        </PrivyProvider>
-    );
+  return (
+    <PrivyProvider
+      appId="cm31r4s7n04ed120svzg37zc3"
+      config={{
+        appearance: {
+          theme: "dark",
+          accentColor: "#676FFF",
+          logo: "https://your-logo-url",
+        },
+        supportedChains: [fraxtalTestnet],
+        intl: { defaultCountry: "IN" },
+        loginMethods: [
+          "email",
+          "wallet",
+          "google",
+          "apple",
+          // "telegram",
+          "discord",
+          "twitter",
+          "farcaster",
+        ],
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
 }
