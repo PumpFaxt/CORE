eaeb3c4019276bb54e24810b671fef2d7c88b526
Spandan Barve <spandan567@gmail.com>
2024-11-05 01:07:43 +0530

    Add generic separator and Divider compoennts

---
+++ packages/client/src/shared/components/Divider.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
+interface IDividerProps {
+    className?: string;
+    children?: preact.VNode;
+}
+
+export default function Divider(props: IDividerProps) {
+    return (
+        <figure className={"flex items-center"} role="separator">
+            <span
+                className={twMerge(
+                    "h-2px flex-1 bg-muted",
+                    props.className,
+                )}
+            />
+            {props.children}
+            <span
+                className={twMerge(
+                    "h-2px flex-1 bg-muted",
+                    props.className,
+                )}
+            />
+        </figure>
+    );
+}
