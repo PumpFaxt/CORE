a499a4113887cc9207f6fcf8791f63db278122de
Spandan Barve <spandan567@gmail.com>
2024-11-20 15:47:27 +0530

    sync commit

--- packages/contracts/environment/localnode.ts
+++ packages/contracts/environment/localnode.ts
@@ -5,45 +5,25 @@ import environmentConfig from "../environment.config.ts";
 import { cliError, consoleFmt } from "../utils.ts";
 import { spawn } from "node:child_process";
 
-async function windowsNohup() {
-  // setTimeout(() => {
-  //   const process = spawn(
-  //     "nohup",
-  //     ["cmd", "/c", "start", "../../bin/anvil.exe"],
-  //     { stdio: "ignore" },
-  //   );
-  // }, 0);
-  const worker = new Worker(new URL("worker.ts", import.meta.url), {
-    type: "module", // Specify module type for Deno
-  });
-
-  // console.log(`Started Anvil with PID: ${process.pid}`);
-}
-
 async function main() {
   const os = Deno.build.os;
 
   if (os === "linux" || os === "darwin") {
+  } else if (os === "windows") {
     console.log(
       consoleFmt.magenta(
-        "NOTE TO SELF : TODO : Add localnode support for Linux and MacOS",
+        "NOTE TO SELF : TODO : Add localnode support for Windows\nWorkaround : deno task win-node.",
       ),
     );
-  } else if (os === "windows") {
-    new Worker(new URL("windows-localnode-worker.ts", import.meta.url), {
-      type: "module", // Specify module type for Deno
-    });
-
-    console.log("first");
   } else {
     cliError("Unsupported OS");
   }
 
-  const admin = createTestClient({
-    mode: "anvil",
-    transport: http(),
-    chain: anvil,
-  });
+  // const admin = createTestClient({
+  //   mode: "anvil",
+  //   transport: http(),
+  //   chain: anvil,
+  // });
 
   // environmentConfig.accounts.forEach(async (key) => {
   //   await admin.setBalance({
@@ -58,5 +38,6 @@ function sleep(ms: number): Promise<void> {
 }
 
 if (import.meta.main) {
+  console.log(`Detected ${Deno.build.os}`);
   main();
 }
--- packages/contracts/environment/windows-localnode-worker.js
+++ packages/contracts/environment/windows-localnode-worker.js
@@ -1,9 +1,11 @@
 import { spawn } from "node:child_process";
 
-self.onmessage = (event) => {
-  const process = spawn(
+self.onmessage = (_) => {
+  spawn(
     "nohup",
     ["cmd", "/c", "start", "../../bin/anvil.exe"],
     { stdio: "ignore" },
   );
+
+  postMessage(0);
 };
