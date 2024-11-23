862f331d77fd9b8fb8eeb4037d47b91a9a11884c
Riya Jain <riya1jain567@gmail.com>
2024-11-23 19:42:13 +0000

    Add header and login banner on discover page

--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -2,17 +2,17 @@ import { ErrorBoundary, Route, Router as PreactRouter } from "preact-iso";
 import _404 from "./pages/_404.tsx";
 import Login from "./pages/login/page.tsx";
 import Discover from "./pages/discover/page.tsx";
-import Layout from "./layout.tsx";
-import ProtectedRoute from "./shared/components/ProtectedRoute.tsx";
+import Layout from "./Layout.tsx";
+// import ProtectedRoute from "./shared/components/ProtectedRoute.tsx";
 
 export default function Router() {
   return (
     <Layout>
       <ErrorBoundary onError={(e) => console.error(e)}>
         <PreactRouter>
-          <ProtectedRoute type="UnauthenticatedOnly">
-            <Route path="login" component={Login} />
-          </ProtectedRoute>
+          {/* <ProtectedRoute type="UnauthenticatedOnly"> */}
+          <Route path="login" component={Login} />
+          {/* </ProtectedRoute> */}
 
           <Route path="discover" component={Discover} />
 
